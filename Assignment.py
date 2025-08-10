a13fc3e3e9819acc0fb9a008c24a4e1dd2dbdb48
Haliyat <adeniyihaliyat716@gmail.com>
2025-08-10 09:10:42 +0100

    Created a list of numbers

---
+++ Assignment.py
@@ -0,0 +1,11 @@
+my_list=[] #created an empty list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50,60,70])
+my_list.remove(70) # removed the last number
+my_list.sort
+index_30= my_list.index(30) #printed the index of ''30
+print (f"Index of 30 : {index_30}")
